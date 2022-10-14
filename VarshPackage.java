1265e912d4e75c4c0180e5ba29bcc93c1e297b06
Raman Malik <63793398+raman2603@users.noreply.github.com>
2022-10-14 14:33:39 +0530

    ..

--- VarshPackage.java
+++ VarshPackage.java
@@ -4,29 +4,28 @@ public class VarshPackage {
 	public String package_id;
 	public String source_place;
 	public String destination_place;
-	public int no_of_days;
+	public int number_of_days;
 	double basic_fare;
 	public double package_cost;
-	public VarshPackage(String package_id, String source_place, String destination_place, int no_of_days,
+	public VarshPackage(String package_id, String source_place, String destination_place, int number_of_days,
 			double basic_fare) {
 		super();
 		this.package_id = package_id;
 		this.source_place = source_place;
 		this.destination_place = destination_place;
-		this.no_of_days = no_of_days;
+		this.number_of_days = number_of_days;
 		this.basic_fare = basic_fare;
 	}
 	
 public void calculatePacakgeCost() {
-	if(this.no_of_days<=5) {this.package_cost= this.basic_fare*this.no_of_days;
+	if(this.number_of_days<=5) {this.package_cost= this.basic_fare*this.number_of_days;
 	return;}
-	if(this.no_of_days>5 && this.no_of_days<=8) {this.package_cost= this.basic_fare*this.no_of_days*0.97;
+	if(this.number_of_days>5 && this.number_of_days<=8) {this.package_cost= this.basic_fare*this.number_of_days*0.97;
 	return;}
-	if(this.no_of_days>8 && this.no_of_days<=10) {this.package_cost= this.basic_fare*this.no_of_days*0.95;
+	if(this.number_of_days>8 && this.number_of_days<=10) {this.package_cost= this.basic_fare*this.number_of_days*0.95;
 	return;}
-	if(this.no_of_days>5) {this.package_cost= this.basic_fare*this.no_of_days*0.93;
-	return;}
-	
+	if(this.number_of_days>5) {this.package_cost= this.basic_fare*this.number_of_days*0.93;
+	return;}	
 	this.package_cost*=1.12;
 }
 }
